0e892993b562542f5936baa64f14ea41d1da9f59
William Due <williamdue@gmail.com>
2021-01-20 00:57:24 +0100

    Updated directory names to CamelCase, namspaces to uppercase and initial work on the database

---
+++ Dikubot/Main.cs
@@ -0,0 +1,42 @@
+using Dikubot.Discord;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dikubot.Webapp;
+using MongoDB.Driver;
+
+namespace Dikubot
+{
+    public class main
+    {
+        public static Thread discordThread;
+        public static void Main(string[] args)
+        {
+            Database.Database.GetInstance.GetDatabase("test");
+            MongoClient MongoClient = new MongoClient("mongodb://localhost:27017");
+            DiscordBot discordBot = new DiscordBot();
+            discordThread = new Thread(new ThreadStart(discordBot.run));
+            discordThread.Start();
+            
+            
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    var p = System.Reflection.Assembly.GetEntryAssembly().Location;
+                    p = p.Substring(0, p.IndexOf("bin")) + "Webapp";
+
+                    webBuilder.UseContentRoot(p);
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
