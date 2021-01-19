7d1fafe50dde638b02ba3031e2743bceb6f40dbc
Lukas <lukiwokimc@gmail.com>
2021-01-19 22:28:57 +0100

    Add project files.

---
+++ dikubot/discord/DiscordBot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace dikubot.discord
+{
+    public class DiscordBot
+    {
+
+		public void run()
+		{
+			MainAsync().GetAwaiter().GetResult();
+		}
+
+		public static DiscordSocketClient client;
+
+		private Task Log(LogMessage msg)
+		{
+			Console.WriteLine(msg.ToString());
+			return Task.CompletedTask;
+		}
+
+		private async Task MessageReceived(SocketMessage message)
+		{
+
+			Console.WriteLine($"({message.Timestamp.ToString()}) {message.Author.ToString()} > {message.ToString()}");
+		}
+
+		public async Task MainAsync()
+		{
+
+			var config = new DiscordSocketConfig
+			{
+				AlwaysDownloadUsers = true,
+				MessageCacheSize = 1000
+			};
+
+
+			client = new DiscordSocketClient(config);
+			string token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+
+			client.Log += Log;
+			client.MessageReceived += MessageReceived;
+
+			await client.LoginAsync(TokenType.Bot, token);
+			await client.StartAsync();
+
+			// Block this task until the program is closed.
+			await Task.Delay(-1);
+		}
+	}
+}
