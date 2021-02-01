c6755d5a95b88681e10d3df15db18bee94700de4
Lukires <lukas.thomas.schilling@gmail.com>
2021-02-01 23:19:29 +0100

    Course progress

---
+++ Dikubot/DataLayer/Database/Models/Course/Services.cs
@@ -0,0 +1,9 @@
+using MongoDB.Driver;
+
+namespace Dikubot.Database.Models.Course
+{
+    public class CourseServices : Services<UserModel>
+    {
+        public CourseServices() : base("Main", "Courses") { }
+    }
+}
