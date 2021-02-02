5a5f509e75beb2ef838fc60a7947d49e5652396c
Lukires <lukas.thomas.schilling@gmail.com>
2021-02-02 10:43:57 +0100

    Course database refactoring

--- Dikubot/DataLayer/Database/Models/Course/Model.cs
+++ Dikubot/DataLayer/Database/Models/Course/Model.cs
@@ -1,9 +1,13 @@
+using System;
+using Dikubot.Database.Models.TextChannel;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace Dikubot.Database.Models.Course
 {
     public class CourseModel : Model
     {
+
+        private static TextChannelServices _textChannelServices = new TextChannelServices();
 
         [BsonElement("Name")] [BsonUnique]
         public string Name
@@ -12,20 +16,26 @@ namespace Dikubot.Database.Models.Course
             set;
         }
         
-        [BsonElement("MainCategoryId")] 
-        public string MainCategoryId { get; set; }
+        [BsonElement("MainTextChannelModelId")] 
+        public Guid MainTextChannelModelId { get; set; }
+
+        [BsonIgnore]
+        public TextChannelModel MainTextChannelModel
+        {
+            get => _textChannelServices.Get(MainTextChannelModelId);
+            set => MainTextChannelModelId = value.Id;
+        }
         
         [BsonElement("TeamCategoryIdList")]
         public string[] TeamCategoryIdList { get; set; }
         
         //MemberRoleModel is the role we will assign to the students enrolled in the course
         [BsonElement("MemberRoleModelId")]
-        public string MemberRoleModelId { get; set; }
-        
-        
+        public Guid MemberRoleModelId { get; set; }
+
         //AdminRoleModel is the role we will assign to the TA
         [BsonElement("AdminRoleModelId")]
-        public string AdminRoleModelId { get; set; }
+        public Guid AdminRoleModelId { get; set; }
         
     }
 }
