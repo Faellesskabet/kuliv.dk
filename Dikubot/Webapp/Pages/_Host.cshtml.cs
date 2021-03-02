fe59505f91281525c4bee62d6a505b72f2d1a62c
Arnvig <bbgG89Hdqu>
2021-03-03 00:03:36 +0100

    Slettet nogle filer som bare vil overs

---
+++ Dikubot/Webapp/Pages/_Host.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Dikubot.Pages
+{
+    public class _HostModel : PageModel
+    {
+        
+        [FromQuery(Name = "code")]
+        public string AuthCode { get; set; }
+
+        public void OnGet()
+        {
+           
+        }
+    }
+}
+
