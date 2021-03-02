13d3ebc39ea36eabc1ff26a87dc8dcf885177872
Lukires <lukas.thomas.schilling@gmail.com>
2021-03-02 14:04:25 +0100

    Data pull on boot and join

--- Dikubot/DataLayer/Logic/User/KUUser.cs
+++ Dikubot/DataLayer/Logic/User/KUUser.cs
@@ -23,7 +23,10 @@ namespace Dikubot.DataLayer.Logic.User
                 content = content.Substring(content.IndexOf("X-WR-CALNAME:") + "X-WR-CALNAME:".Length);
                 int from = content.IndexOf("-") + "-".Length;
                 int to = content.IndexOf("BEGIN:");
-                return content.Substring(from, to - from).Trim();
+                string name = content.Substring(from, to - from).Trim();
+
+                return NameSanityCheck(name) ? name : "";
+
             }
             catch (Exception e)
             {
@@ -31,6 +34,42 @@ namespace Dikubot.DataLayer.Logic.User
             }
 
             return "";
+        }
+
+        
+        /// <summary>
+        /// We simply check whether or not the name is somewhat reasonable
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Reeturns true if it passes the sanity check</returns>
+        private bool NameSanityCheck(string name)
+        {
+            /*
+                Discord enforces the following restrictions for usernames and nicknames:
+
+                Names can contain most valid unicode characters. We limit some zero-width and non-rendering characters.
+                Usernames must be between 2 and 32 characters long.
+                Nicknames must be between 1 and 32 characters long.
+                Names are sanitized and trimmed of leading, trailing, and excessive internal whitespace.
+
+                The following restrictions are additionally enforced for usernames:
+
+                Names cannot contain the following substrings: '@', '#', ':', '```'.
+                Names cannot be: 'discordtag', 'everyone', 'here'.
+            */
+            
+            //KU usernames are 6 characters, so we won't allow names under that
+            if (name.Length < 6 || name.Length > 32)
+            {
+                return false;
+            }
+
+            if (name.Contains("@") || name.Contains("#") || name.Contains(":") || name.Contains("```"))
+            {
+                return false;
+            }
+
+            return name != "discordtag" && name != "everyone" && name != "here";
         }
         
         
---
+++ Dikubot/Discord/EventListeners/GuildDownloadListeners.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Dikubot.DataLayer.Static;
+using Dikubot.Permissions;
+using Discord.WebSocket;
+
+namespace Dikubot.Discord.EventListeners
+{
+    public class GuildDownloadListeners
+    {
+        public async Task DownloadGuildOnBoot()
+        {
+            foreach (var guild in DiscordBot.client.Guilds)
+            {
+                Logger.Debug($"Pulling Discord data into Database for {guild.Name} ({guild.Id.ToString()})");
+                PermissionsService permissionsService = new PermissionsService(guild);
+                permissionsService.SetDatabase();
+                Logger.Debug($"Successfully pulled Discord data into Database for {guild.Name} ({guild.Id.ToString()})");
+            }
+        }
+
+        public async Task DownloadGuildOnJoin(SocketGuild guild)
+        {
+            Logger.Debug($"Pulling Discord data into Database for {guild.Name} ({guild.Id.ToString()})");
+            PermissionsService permissionsService = new PermissionsService(guild);
+            permissionsService.SetDatabase();
+            Logger.Debug($"Successfully pulled Discord data into Database for {guild.Name} ({guild.Id.ToString()})");
+        }
+    }
+}
