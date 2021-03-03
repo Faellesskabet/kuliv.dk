4caa36e07ee7524c3081cb3674a2b3165b8df371
Lukires <lukas.thomas.schilling@gmail.com>
2021-03-03 18:30:43 +0100

    Group and course progress

--- Dikubot/DataLayer/Database/Models/Group/Services.cs
+++ Dikubot/DataLayer/Database/Models/Group/Services.cs
@@ -1,8 +1,9 @@
+using Dikubot.Database.Models.Group;
 using Discord.WebSocket;
 
 namespace Dikubot.Database.Models.Education
 {
-    public class EducationServices : Services<EducationModel>
+    public class EducationServices : Services<GroupModel>
     {
         public EducationServices(SocketGuild guild) : base("Main", "Education", guild)
         {
