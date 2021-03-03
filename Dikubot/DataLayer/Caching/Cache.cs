bb54010022caf92e73aa8639d8d71ecd9f4f790e
Lukires <lukas.thomas.schilling@gmail.com>
2021-03-03 01:38:06 +0100

    Simple cache system

---
+++ Dikubot/DataLayer/Caching/Cache.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cronos;
+using Dikubot.DataLayer.Cronjob;
+using Microsoft.Graph;
+
+namespace Dikubot.DataLayer.Caching
+{
+    /// <summary>
+    /// This cache is unreliable in the sense that items are not cached individually, meaning items are cached between x and y minutes, instead of a constant amount of time.
+    /// The reason the cache is implemented like this is really just because I thought it was a fun way of implementing cache
+    /// </summary>
+    public class Cache<TKey, TValue>
+    {
+        private int _swap;
+        private CronExpression _cronExpression;
+        private CronTask _cronTask;
+        private Scheduler _scheduler = new Scheduler();
+
+        
+        private Dictionary<TKey, TValue> activeStorage = new Dictionary<TKey, TValue>();
+        private Dictionary<TKey, TValue> inactiveStorage = new Dictionary<TKey, TValue>();
+
+        /// <summary>
+        /// Initialize a cache. Cached items will be cached for at least swap minutes, and at most 2 times swap minutes
+        /// </summary>
+        /// <param name="swap">Cached items will be cached for at least swap minutes, and at most 2 times swap minutes</param>
+        public Cache(int swap)
+        {
+            _swap = swap;
+            _cronTask = new CronTask(CronExpression.Parse($"*/{swap} * * * *"), Swap);
+
+        }
+        
+        /// <summary>
+        /// Swap simply switches the inactive and active storage
+        /// The active storage is the storage items is added to, until the next swap, while the inactive storage gets deleted at the next swap
+        /// </summary>
+        private void Swap()
+        {
+            inactiveStorage = new Dictionary<TKey, TValue>(activeStorage);
+            activeStorage.Clear();
+        }
+
+        protected virtual TValue GetValue(TKey key)
+        {
+            if (activeStorage.ContainsKey(key))
+            {
+                return activeStorage[key];
+            }
+            return inactiveStorage[key];
+        }
+
+        protected virtual void SetValue(TKey key, TValue value)
+        {
+            activeStorage[key] = value;
+        }
+        
+
+        public void Add(TKey key, TValue value)
+        {
+            SetValue(key, value);
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            return activeStorage.ContainsKey(key) || inactiveStorage.ContainsKey(key);
+        }
+
+        public void Remove(TKey key)
+        {
+            if (activeStorage.ContainsKey(key))
+            {
+                activeStorage.Remove(key);
+                return;
+            }
+            inactiveStorage.Remove(key);
+        }
+
+        public TValue this[TKey key]
+        {
+            get => GetValue(key);
+            set => SetValue(key, value);
+        }
+        
+        
+        
+        
+    }
+}
