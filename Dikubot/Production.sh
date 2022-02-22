91cef428d77e0a5ddafdc156744725c48128a5ec
Lukires <lukas.thomas.schilling@gmail.com>
2022-02-22 13:51:27 +0100

    Production scripts and fixes

---
+++ Dikubot/Production.sh
@@ -0,0 +1,5 @@
+sudo systemctl stop kestrel-bot.service
+dotnet publish --configuration Release
+rm -rf ./bin/Release/net6.0/wwwroot
+cp -r Webapp/wwwroot/ ./bin/Release/net6.0
+sudo systemctl start kestrel-bot.service
--- Dikubot/Webapp/Startup.cs
+++ Dikubot/Webapp/Startup.cs
@@ -54,8 +54,7 @@ namespace Dikubot.Webapp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
