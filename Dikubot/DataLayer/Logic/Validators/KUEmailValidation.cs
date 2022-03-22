a415701a37becd6249796a68e827cb4eb068a115
Lukires <lukas.thomas.schilling@gmail.com>
2022-03-22 23:54:11 +0100

    Email validation messages displayed

--- Dikubot/DataLayer/Logic/Validators/KUEmailValidation.cs
+++ Dikubot/DataLayer/Logic/Validators/KUEmailValidation.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Dikubot.DataLayer.Static;
 using FluentValidation;
 
@@ -10,4 +13,14 @@ public class KUEmailValidation : AbstractValidator<string>
         RuleFor(s => s).EmailAddress().WithMessage("Ikke en gyldig email");
         RuleFor(s => s).Must(Util.IsKUEmail).WithMessage("Ikke en gyldig KU email");
     }
+    
+    private IEnumerable<string> ValidateValue(string arg)
+    {
+        var result = Validate(arg);
+        if (result.IsValid)
+            return new string[0];
+        return result.Errors.Select(e => e.ErrorMessage);
+    }
+
+    public Func<string, IEnumerable<string>> Validation => ValidateValue;
 }
