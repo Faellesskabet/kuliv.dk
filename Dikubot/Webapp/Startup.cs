1ffe4698c8d8b1c85c88eb546ad32dc2b6dfa7ba
Lukires <lukas.thomas.schilling@gmail.com>
2022-03-31 01:33:19 +0200

    debugging on production

--- Dikubot/Webapp/Startup.cs
+++ Dikubot/Webapp/Startup.cs
@@ -102,7 +102,7 @@ namespace Dikubot.Webapp
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             
             app.UseStaticFiles();
