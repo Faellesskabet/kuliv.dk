b67d3f4edfd766db80978d0b744cea65f274e25c
Lukires <lukas.thomas.schilling@gmail.com>
2022-03-31 00:01:09 +0200

    Updated production script

--- Dikubot/Production.sh
+++ Dikubot/Production.sh
@@ -1,4 +1,6 @@
 sudo systemctl stop kestrel-bot.service
+mv ./bin/Release/net6.0/Backups/ ../../Discord-Backups/
+sudo rm -rf ./bin/Release/net6.0
 dotnet publish --configuration Release
 rm -rf ./bin/Release/net6.0/wwwroot
 cp -r Webapp/wwwroot/ ./bin/Release/net6.0
