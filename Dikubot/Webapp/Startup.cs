a171aefb5c22ee6abd9313e0c4e174e524513717
Lukires <lukas.thomas.schilling@gmail.com>
2022-07-20 01:44:33 +0200

    hhhm

--- Dikubot/Webapp/Startup.cs
+++ Dikubot/Webapp/Startup.cs
@@ -84,9 +84,7 @@ namespace Dikubot.Webapp
 
             services.AddAuthentication(options =>
                 {
-                    ///CookieAuthenticationDefaults.AuthenticationScheme
                     options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                     options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 })
                 .AddCookie(options =>
@@ -119,9 +117,10 @@ namespace Dikubot.Webapp
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseExceptionHandler("/Error");
+                app.UseDeveloperExceptionPage();
+                //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
             }
             
             app.UseStaticFiles();
