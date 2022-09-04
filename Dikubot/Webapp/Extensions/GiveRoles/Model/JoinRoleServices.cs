1b0194e57e1279c6df5d756e26a028126ae5a9f5
Arnvig <bbgG89Hdqu>
2022-09-04 16:18:45 +0200

    Udstyrkalender EmojiSelector GiveRoles (Opdateret med Emojiseletor) Tags - Nu kan Super Ops lave Tags NavMenu - Sat bedre op og lave en "Hurtig knap til at skifte fælleskab" AddGuild - Gør det lidt nemmere at tilføje Guilds GDPR - Skrev en helt masse Volenteer - Skrev En helt masse

---
+++ Dikubot/Webapp/Extensions/GiveRoles/Model/JoinRoleServices.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Dikubot.DataLayer.Database.Guild.Models.Role;
+using Dikubot.Discord;
+using Discord.WebSocket;
+using MongoDB.Driver;
+
+namespace Dikubot.DataLayer.Database.Guild.Models.JoinRole
+{
+    public class JoinRoleServices : GuildServices<JoinRoleCategoryMainModel>
+    { 
+        public JoinRoleServices(SocketGuild guild) : base("JoinRole", guild)
+        {
+        }
+        
+        public JoinRoleServices(string guidId) : base("JoinRole", DiscordBot.Client.Guilds?.FirstOrDefault(g => g.Id.ToString().Equals(guidId)))
+        {
+        }
+        
+    }
+
+}
