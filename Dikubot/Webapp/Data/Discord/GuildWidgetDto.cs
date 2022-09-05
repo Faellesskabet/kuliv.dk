7f71fea0c692bc10d6eb281397a51511a7f32b9e
Lukas Schilling <lukas_schilling@balticfinance.com>
2022-09-06 02:40:56 +0300

    Changed look and feel of guild join site

---
+++ Dikubot/Webapp/Data/Discord/GuildWidgetDto.cs
@@ -0,0 +1,16 @@
+using Dikubot.DataLayer.Database.Global.GuildSettings;
+
+namespace Data.Discord;
+
+public class GuildWidgetDto
+{
+    public GuildWidgetDto(GuildSettingsModel guildSettingsModel, bool isUserMember)
+    {
+        GuildSettingsModel = guildSettingsModel;
+        IsUserMember = isUserMember;
+    }
+
+    public GuildSettingsModel GuildSettingsModel { get; }
+
+    public bool IsUserMember { get; }
+}
