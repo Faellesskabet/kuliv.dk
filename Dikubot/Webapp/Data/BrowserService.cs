a04675135dbced0f99fd3908a74476b46eec6397
Lukas Schilling <lukas_schilling@balticfinance.com>
2022-10-06 01:24:45 +0300

    Remove navbar on smaller windows

---
+++ Dikubot/Webapp/Data/BrowserService.cs
@@ -0,0 +1,26 @@
+namespace Data;
+
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+public class BrowserService
+{
+    private readonly IJSRuntime _js;
+
+    public BrowserService(IJSRuntime js)
+    {
+        _js = js;
+    }
+
+    public async Task<BrowserDimension> GetDimensions()
+    {
+        return await _js.InvokeAsync<BrowserDimension>("getDimensions");
+    }
+
+}
+
+public class BrowserDimension
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+}
