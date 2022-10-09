5d0b9d4ac66c50d7f675250fca7e0bc30b8ec202
Arnvig <bbgG89Hdqu>
2022-10-09 22:49:19 +0200

    Events er næsten færdig :D Pusher det som fungere :D

--- Dikubot/Webapp/Extensions/Calendar/Model/CalenderServices.cs
+++ Dikubot/Webapp/Extensions/Calendar/Model/CalenderServices.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Dikubot.DataLayer.Database.Global.User;
 using Dikubot.DataLayer.Database.Guild.Models.Calendar.Events;
 using Dikubot.Discord;
 using Discord.WebSocket;
@@ -10,17 +11,18 @@ namespace Dikubot.DataLayer.Database.Guild.Models.Calendar
 {//https://stackoverflow.com/questions/13905359/cannot-provide-arguments-when-creating-an-instance-of-generic-type
     public class CalendarServices : GuildServices<CalendarModel>
     {
-        
+        private ulong? guildId = null;
         public CalendarServices(SocketGuild guild) : base("Calendar", guild)
         {
+            guildId = guild.Id;
         }
         
-
-        public CalendarServices(string guidId) : base("Calendar", DiscordBot.ClientStatic.Guilds?.FirstOrDefault(g => g.Id.ToString().Equals(guidId)))
+        
+        public CalendarServices(string guidId) : base("Calendar", DiscordBot.ClientStatic?.Guilds?.First(g => g.Id.ToString().Equals(guidId)))
         {
+            guildId = Convert.ToUInt64(guidId);
         }
-
-
+        
         public List<EventModel> GetEvents(Expression<Func<CalendarModel, bool>> calFilter = null, Expression<Func<EventModel, bool>> eventFilter = null)
         {
             var res = new List<EventModel>();
@@ -31,11 +33,71 @@ namespace Dikubot.DataLayer.Database.Guild.Models.Calendar
             {
                 foreach (var model in cal)
                 {
-                    res.AddRange(eventsServices.GetAllAsDictionary(eventFilter).SelectMany(x => x.Value).Where(e => e.Calendars.Contains(model.Id.ToString())));
+                    res.AddRange(eventsServices.GetAllAsDictionary(eventFilter).SelectMany(x => x.Value).Where(e => e.Calendars.Contains(model.Id)));
                 }
             }
             return res;
         }
+
+        public List<CalendarModel> Get(CalendarModel.EnumCalendarType calendarType, UserGlobalModel user)
+        {
+            var roles = guildId.HasValue ? user.GetRolesGuid((ulong)guildId) : user.GetRolesGuid();
+        
+            return GetAll(model => model.CalendarType == calendarType)
+                .FindAll(model => model.Visible != CalendarModel.EnumAvailable.Privat 
+                                  || model.Permission.Overlaps(roles)
+                                  || model.View.Overlaps(roles));
+        }
+
+
+        public new static List<(SocketGuild,List<CalendarModel>)> GetAllViewCalenders(CalendarModel.EnumCalendarType calendarType, UserGlobalModel user)
+        {
+            var roles = user?.GetRolesGuid() ?? new HashSet<Guid>();
+
+            List<(SocketGuild,List<CalendarModel>)> result = new List<(SocketGuild, List<CalendarModel>)>();
+            
+            foreach (var socketGuild in DiscordBot.ClientStatic.Guilds)
+            {
+                var service = new CalendarServices(socketGuild);
+            
+                List<CalendarModel> value = service.GetAll(model => model.CalendarType == calendarType).Where(model => 
+                    model.Visible == CalendarModel.EnumAvailable.Public 
+                    || (user.Verified && model.Visible == CalendarModel.EnumAvailable.Externt)
+                    || (user.Guilds.Contains(socketGuild) && model.Visible == CalendarModel.EnumAvailable.Internt)
+                    || model.Permission.Overlaps(roles)
+                    || model.View.Overlaps(roles)).ToList();
+                result.Add((socketGuild,value));
+            }
+            return result;
+        }
+        
+        
+        public new static List<(SocketGuild,List<CalendarModel>)> GetAllPermisionsCalendars(CalendarModel.EnumCalendarType calendarType, UserGlobalModel user)
+        {
+            if (user is null)
+            {
+                return new List<(SocketGuild, List<CalendarModel>)>();
+            }
+            
+            var roles = user.GetRolesGuid();
+
+            List<(SocketGuild,List<CalendarModel>)> result = new List<(SocketGuild, List<CalendarModel>)>();
+            
+            
+            foreach (var socketGuild in user.Guilds)
+            {
+                var service = new CalendarServices(socketGuild);
+            
+                List<CalendarModel> value = service.GetAll(model => model.CalendarType == calendarType).Where(model => model.Permission.Overlaps(roles)).ToList();
+                result.Add((socketGuild,value));
+                 
+                
+            }
+
+            return result;
+
+        }
+        
     }
 
    
--- Dikubot/Webapp/Extensions/Calendar/Model/Events/EventServices.cs
+++ Dikubot/Webapp/Extensions/Calendar/Model/Events/EventServices.cs
@@ -1,34 +1,95 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using Data;
 using Dikubot.DataLayer.Database.Global;
 using Dikubot.DataLayer.Database.Global.Settings.Tags;
 using Dikubot.Discord;
 using Discord.WebSocket;
+using Microsoft.IdentityModel.Tokens;
 using MongoDB.Driver;
+
 
 namespace Dikubot.DataLayer.Database.Guild.Models.Calendar.Events
 {
     public class EventsServices : GlobalServices<EventModel>
     {
-        private string _calendar;
-        
-        public EventsServices(SocketGuild guildId, string calendar = null) : base("Events")
+        private readonly Guid? _calendar;
+        private string guildId;
+
+        public EventsServices(SocketGuild guildId, Guid? calendar =null) : base("Events")
         {
             _calendar = calendar;
+            this.guildId = guildId?.Id.ToString();
+
         }
         
-        public EventsServices(string guildId = null, string calendar = null) : base("Events")
+        public EventsServices(string guildId = null, Guid? calendar = null) : base("Events")
         {
             _calendar = calendar;
+            this.guildId = guildId;
         }
         
         public override List<EventModel> Get()
         {
-            if (string.IsNullOrWhiteSpace(_calendar))
+            if (_calendar != null)
             {
                 return base.Get();
             }
-            return _models.Find(e => e.Calendars.Equals(_calendar)).ToList();
+            return _models.Find(e => e.Calendars.Contains(_calendar ?? Guid.Empty)).ToList();
+        }
+
+
+        public List<EventModel> Get(DateTime currentDate, UserService userService = null)
+        {
+            return Get(new DateTime(currentDate.Year,currentDate.Month, 1), 
+                new DateTime(currentDate.Year,currentDate.Month, 
+                    DateTime.DaysInMonth(currentDate.Year,currentDate.Month),
+                    23,59,59), 
+                userService);
+        }
+        
+        public List<EventModel> Get(DateTime startTime, DateTime endTime, UserService userService = null)
+        {
+            HashSet<Guid> roles = new HashSet<Guid>();
+            if (userService != null)
+            {
+                roles = !guildId.IsNullOrEmpty()
+                    ? userService.GetUserGlobalModel().GetRolesGuid(Convert.ToUInt64(guildId))
+                    : userService.GetUserGlobalModel().GetRolesGuid();
+            }
+
+            List<CalendarModel> calendarModels = !guildId.IsNullOrEmpty()
+                ? new CalendarServices(this.guildId)
+                .Get(CalendarModel.EnumCalendarType.Event, userService.GetUserGlobalModel()) : new List<CalendarModel>();
+
+            HashSet<Guid> ViewCalenders = CalendarServices
+                .GetAllViewCalenders(CalendarModel.EnumCalendarType.Event, userService.GetUserGlobalModel())
+                .SelectMany(m => m.Item2).Select(m => m.Id).ToHashSet();
+            
+            HashSet<Guid> PermisionsCalenders = CalendarServices
+                .GetAllPermisionsCalendars(CalendarModel.EnumCalendarType.Event, userService.GetUserGlobalModel())
+                .SelectMany(m => m.Item2).Select(m => m.Id).ToHashSet(); 
+            
+            List<EventModel> result = GetAll(model => (model.StartTime.CompareTo(startTime) >= 0 &&
+                                                                model.StartTime.CompareTo(endTime) <= 0)
+                                                                || (model.EndTime.CompareTo(startTime) >= 0
+                                                                    && model.EndTime.CompareTo(endTime) <= 0) ||
+                                                                (model.StartTime.CompareTo(startTime) <= 0 && 
+                                                                 model.EndTime.CompareTo(endTime) >= 0)
+                ).Where(model => model.Calendars.Count == 0
+                                 || PermisionsCalenders.Overlaps(model.Calendars) ||
+                                 ViewCalenders.Overlaps(model.Calendars)
+                                 ).Select(m =>
+                {
+                    m.IsReadonly = userService != null && !( m.Hosts.Contains(userService.GetUserGlobalModel().DiscordId) 
+                                                           || PermisionsCalenders.Overlaps(m.Calendars));
+                    return m;
+                })
+                .ToList();
+            result.Sort((x, y) => DateTime.Compare(x.StartTime, y.StartTime));
+
+            return result;
         }
         
     }
