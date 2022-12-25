a74d991f9f2b19171477dbfbcd40699c9ad44999
Lukires <lukas.thomas.schilling@gmail.com>
2022-12-25 16:57:43 +0200

    Expand verify user hotfix

--- Dikubot/Discord/Command/VerifyUser.cs
+++ Dikubot/Discord/Command/VerifyUser.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Dikubot.DataLayer.Database.Global.User;
+using Dikubot.DataLayer.Database.Guild.Models.User;
+using Dikubot.DataLayer.Permissions;
 using Dikubot.Discord.EventListeners;
 using Discord;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace Dikubot.Discord.Command;
 
@@ -38,6 +42,20 @@ public class VerifyUser : ModuleBase<SocketCommandContext>
         userModel.Email = email;
         userModel.Verified = true;
         userGlobalServices.Upsert(userModel);
+        
+        IReadOnlyCollection<SocketGuild> mutualGuilds = DiscordBot.ClientStatic.GetUser(userModel.DiscordIdLong)?.MutualGuilds;
+        if (mutualGuilds != null)
+        {
+            foreach (SocketGuild mutualGuild in mutualGuilds)
+            {
+                UserGuildServices userGuildServices = new UserGuildServices(mutualGuild);
+                PermissionsService permissionsService = new PermissionsService(mutualGuild);
+                UserGuildModel newUser = userGuildServices.Get(userModel.DiscordId);
+                await permissionsService.SetDiscordUserRoles(newUser);
+            }
+        }
+        
+        
         await ReplyAsync($"User with ID {user.Id} and name {user.Username} has been verified with email ${email}");
     }
 }
